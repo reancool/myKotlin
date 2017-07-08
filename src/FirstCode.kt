2a21f2bcc62dfcf8961902632cdb80003f0d028d
fly <reancool@163.com>
2017-07-08 22:18:57 +0800

    for ，however “for” postion need to add tag befrore “for"

--- src/FirstCode.kt
+++ src/FirstCode.kt
@@ -3,27 +3,14 @@
  */
 fun main(args: Array<String>)
 {
-    var str: String ?=null;
-//    var titu =Alien();
-    var titu =AlienKt();
-//    println(titu.name?.length)
-//    println(titu.name)
+//    var nums=1 ..  16
+//    for (a in nums){
+////        println(a)
+//    }
 
-
-    val num : Int =2;
-    when (num){
-        1-> println("one")
-        2-> println("two")
-        3-> println("three")
-        else-> println("nothing")
+    var letter='A' ..'z'
+    for ( a in letter){
+        println(a)
     }
 
-    var str2=when(num){
-        1-> "two"
-        2->"three"
-        else -> {
-            "nothing"
-        }
-    }
-    println("str2 is ${str2}")
 }
