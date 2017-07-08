75f838b49359a4107aee91e92083707b07b8c8e1
fly <reancool@163.com>
2017-07-08 22:29:43 +0800

    “for” postion  index use  whitIndex, with  two parameter,  Map print

--- src/FirstCode.kt
+++ src/FirstCode.kt
@@ -1,16 +1,21 @@
+import java.util.*
+
 /**
  * Created by fly on 2017/7/8.
  */
 fun main(args: Array<String>)
 {
-//    var nums=1 ..  16
-//    for (a in nums){
-////        println(a)
-//    }
-
-    var letter='A' ..'z'
-    for ( a in letter){
-        println(a)
+    var nums= listOf(1,2,3)
+    for ((i,e) in nums.withIndex()){
+        println("index--${i}-value-${e}")
     }
 
+    var map = TreeMap<String, Int>()
+    map["Navin"]=543
+    map["Kishor"]=666
+
+    for ((name,age)in map)
+    {
+        println("${name}--${age}")
+    }
 }
