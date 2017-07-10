acf7a144a8e2cf962457a739d7312b17e6560a3d
fly <reancool@163.com>
2017-07-10 14:21:28 +0800

    try catch

--- src/FirstCode.kt
+++ src/FirstCode.kt
@@ -5,12 +5,14 @@ import java.util.*
  */
 fun main(args: Array<String>)
 {
-    var str: String="4"
-    var num:Int=str.toInt()
-    var nums=Integer.parseInt(str)
-    println(num)
-    println(nums)
+    var str: String="4a"
+    var num:Int=
 
+    try {
+       str.toInt()
+    } catch(e: NumberFormatException) {
+        -1
+    }
     num++
     println(num)
 }
