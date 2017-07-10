eb2b1c6b095cbfc8399f47e8bc69bf54f1e46ea2
fly <reancool@163.com>
2017-07-10 14:13:00 +0800

    string to int

--- src/FirstCode.kt
+++ src/FirstCode.kt
@@ -5,13 +5,12 @@ import java.util.*
  */
 fun main(args: Array<String>)
 {
-    var finalAmt=calaAmount(50,0.03)
-    var finalAmt2=calaAmount(interest=0.03,amt=60)
-    println("finalAmt is ${finalAmt}")
-    println("finalAmt2 is ${finalAmt2}")
+    var str: String="4"
+    var num:Int=str.toInt()
+    var nums=Integer.parseInt(str)
+    println(num)
+    println(nums)
 
-}
-@JvmOverloads
-fun calaAmount(amt:Int,interest:Double=0.04) :Double{
-    return amt+amt*interest
+    num++
+    println(num)
 }
