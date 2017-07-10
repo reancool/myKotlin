22474a63dfa948e5d2137cc883fe754b1a5c0ac0
fly <reancool@163.com>
2017-07-10 14:44:27 +0800

    operator infix .operator is smillar to Python

--- src/FirstCode.kt
+++ src/FirstCode.kt
@@ -13,13 +13,31 @@ fun main(args: Array<String>)
     a2.skills="SQL"
     a2.show()
 
-    var a3=a1.plus(a2)
+    var a3=a1.pluss(a2)
     a3.show()
 
+    var a4=a1 plusI a2
+    a4.show()
+
+    var a5=a1 + a2
+    a5.show()
 }
 
-fun AlienKt.plus(a:AlienKt):AlienKt{
+fun AlienKt.pluss(a:AlienKt):AlienKt{
     var newAlien=AlienKt()
     newAlien.skills=this.skills+" "+a.skills
     return newAlien
 }
+
+infix fun AlienKt.plusI(a:AlienKt):AlienKt{
+    var newAlien=AlienKt()
+    newAlien.skills=this.skills+" "+a.skills
+    return newAlien
+}
+operator infix fun AlienKt.plus(a:AlienKt):AlienKt{
+    var newAlien=AlienKt()
+    newAlien.skills=this.skills+" "+a.skills
+    return newAlien
+}
+
+
